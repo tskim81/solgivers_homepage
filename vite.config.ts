e904631dcd61bc12808b9481365afafb4b678585
tskim81 <159745019+tskim81@users.noreply.github.com>
2025-12-14 10:44:15 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/solgivers_homepage/',
+  base: "/",
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -21,3 +20,5 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
+
